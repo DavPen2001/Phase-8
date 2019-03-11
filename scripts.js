df5d84bb769ece0820817ce60402aa5b409e44ab
DavPen2001 <david_rodriguez@davincischools.org>
2019-03-11 13:08:23 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,120 +1,120 @@
 function nextImage(el){
-if (el.src.match("images/one.png")){
-el.src = "images/two.png";
-} else if (el.src.match("images/two.png")){
-el.src = "images/three.png";
-} else if (el.src.match("images/three.png")){
-el.src = "images/four.png";
-} else if (el.src.match("images/four.png")){
-el.src = "images/five.png";
-} else if (el.src.match("images/five.png")){
-el.src = "images/six.png";
-} else if (el.src.match("images/six.png")){
-el.src = "images/seven.png";
-} else if (el.src.match("images/seven.png")){
-el.src = "images/eight.png";
-} else if (el.src.match("images/eight.png")){
-el.src = "images/nine.png";
-} else if (el.src.match("images/nine.png")){
-el.src = "images/zero.png";
-} else if (el.src.match("images/zero.png")){
-el.src = "images/one.png";
+if (el.src.match("one.png")){
+el.src = "two.png";
+} else if (el.src.match("two.png")){
+el.src = "three.png";
+} else if (el.src.match("three.png")){
+el.src = "four.png";
+} else if (el.src.match("four.png")){
+el.src = "five.png";
+} else if (el.src.match("five.png")){
+el.src = "six.png";
+} else if (el.src.match("six.png")){
+el.src = "seven.png";
+} else if (el.src.match("seven.png")){
+el.src = "eight.png";
+} else if (el.src.match("eight.png")){
+el.src = "nine.png";
+} else if (el.src.match("nine.png")){
+el.src = "zero.png";
+} else if (el.src.match("zero.png")){
+el.src = "one.png";
 }
 else {
 }
 }
 
 function nextImage2(el){
-if (el.src.match("images/one.png")){
-el.src = "images/two.png";
-} else if (el.src.match("images/two.png")){
-el.src = "images/three.png";
-} else if (el.src.match("images/three.png")){
-el.src = "images/four.png";
-} else if (el.src.match("images/four.png")){
-el.src = "images/five.png";
-} else if (el.src.match("images/five.png")){
-el.src = "images/six.png";
-} else if (el.src.match("images/six.png")){
-el.src = "images/seven.png";
-} else if (el.src.match("images/seven.png")){
-el.src = "images/eight.png";
-} else if (el.src.match("images/eight.png")){
-el.src = "images/nine.png";
-} else if (el.src.match("images/nine.png")){
-el.src = "images/zero.png";
-} else if (el.src.match("images/zero.png")){
-el.src = "images/one.png";
+if (el.src.match("one.png")){
+el.src = "two.png";
+} else if (el.src.match("two.png")){
+el.src = "three.png";
+} else if (el.src.match("three.png")){
+el.src = "four.png";
+} else if (el.src.match("four.png")){
+el.src = "five.png";
+} else if (el.src.match("five.png")){
+el.src = "six.png";
+} else if (el.src.match("six.png")){
+el.src = "seven.png";
+} else if (el.src.match("seven.png")){
+el.src = "eight.png";
+} else if (el.src.match("eight.png")){
+el.src = "nine.png";
+} else if (el.src.match("nine.png")){
+el.src = "zero.png";
+} else if (el.src.match("zero.png")){
+el.src = "one.png";
 }
 else {
 }
 }
 
 function nextImage3(el){
-if (el.src.match("images/one.png")){
-el.src = "images/two.png";
-} else if (el.src.match("images/two.png")){
-el.src = "images/three.png";
-} else if (el.src.match("images/three.png")){
-el.src = "images/four.png";
-} else if (el.src.match("images/four.png")){
-el.src = "images/five.png";
-} else if (el.src.match("images/five.png")){
-el.src = "images/six.png";
-} else if (el.src.match("images/six.png")){
-el.src = "images/seven.png";
-} else if (el.src.match("images/seven.png")){
-el.src = "images/eight.png";
-} else if (el.src.match("images/eight.png")){
-el.src = "images/nine.png";
-} else if (el.src.match("images/nine.png")){
-el.src = "images/zero.png";
-} else if (el.src.match("images/zero.png")){
-el.src = "images/one.png";
+if (el.src.match("one.png")){
+el.src = "two.png";
+} else if (el.src.match("two.png")){
+el.src = "three.png";
+} else if (el.src.match("three.png")){
+el.src = "four.png";
+} else if (el.src.match("four.png")){
+el.src = "five.png";
+} else if (el.src.match("five.png")){
+el.src = "six.png";
+} else if (el.src.match("six.png")){
+el.src = "seven.png";
+} else if (el.src.match("seven.png")){
+el.src = "eight.png";
+} else if (el.src.match("eight.png")){
+el.src = "nine.png";
+} else if (el.src.match("nine.png")){
+el.src = "zero.png";
+} else if (el.src.match("zero.png")){
+el.src = "one.png";
 }
 else {
 }
 }
 
 function nextImage4(el){
-if (el.src.match("images/turret.png")){
-el.src = "images/cube.png";
-} else if (el.src.match("images/cube.png")){
-el.src = "images/turret2.png";
-} else if (el.src.match("images/turret2.png")){
-el.src = "images/gun.png";
-} else if (el.src.match("images/gun.png")){
-el.src = "images/turret.png";
+if (el.src.match("turret.png")){
+el.src = "cube.png";
+} else if (el.src.match("cube.png")){
+el.src = "turret2.png";
+} else if (el.src.match("turret2.png")){
+el.src = "gun.png";
+} else if (el.src.match("gun.png")){
+el.src = "turret.png";
 }
 else {
 }
 }
 
 function nextImage6(el){
-if (el.src.match("images/turret.png")){
-el.src = "images/cube.png";
-} else if (el.src.match("images/cube.png")){
-el.src = "images/inspect.png";
-} else if (el.src.match("images/inspect.png")){
-el.src = "images/turret2.png";
-} else if (el.src.match("images/turret2.png")){
-el.src = "images/gun.png";
-} else if (el.src.match("images/gun.png")){
-el.src = "images/turret.png";
+if (el.src.match("turret.png")){
+el.src = "cube.png";
+} else if (el.src.match("cube.png")){
+el.src = "inspect.png";
+} else if (el.src.match("inspect.png")){
+el.src = "turret2.png";
+} else if (el.src.match("turret2.png")){
+el.src = "gun.png";
+} else if (el.src.match("gun.png")){
+el.src = "turret.png";
 }
 else {
 }
 }
 
 function nextImage5(el){
-if (el.src.match("images/turret.png")){
-el.src = "images/cube.png";
-} else if (el.src.match("images/cube.png")){
-el.src = "images/turret2.png";
-} else if (el.src.match("images/turret2.png")){
-el.src = "images/gun.png";
-} else if (el.src.match("images/gun.png")){
-el.src = "images/turret.png";
+if (el.src.match("turret.png")){
+el.src = "cube.png";
+} else if (el.src.match("cube.png")){
+el.src = "turret2.png";
+} else if (el.src.match("turret2.png")){
+el.src = "gun.png";
+} else if (el.src.match("gun.png")){
+el.src = "turret.png";
 }
 else {
 }
@@ -126,7 +126,7 @@ c = document.getElementById("three");
 mySound = document.getElementById("portal");
 
 function checkImages(el){
-if (a.src.match("images/one.png") && b.src.match("images/two.png") && c.src.match("images/three.png")){
+if (a.src.match("one.png") && b.src.match("two.png") && c.src.match("three.png")){
 mySound.play();
 setTimeout(redirect, 2000);
 } else {
